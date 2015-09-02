c43f08d2005255f19a54a4a16dc871694939bdc3
Logan Walker <logan.walker@me.com>
2015-09-02 12:41:50 -0400

    Replace HTTP_Post with CURL

--- index.php
+++ index.php
@@ -2,12 +2,19 @@
 
 $botId = "<BOT_ID>";
 
-$quote = "tmp"
+$quote = "a wild test appears";
 $fields = array(
 	'text' => $quote,
 	'bot_id' => $botId
 );
 
-$response = http_post_fields("https://api.groupme.com/v3/bots/post",$fields);
+$ch = curl_init("https://api.groupme.com/v3/bots/post");
+
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+curl_close($ch);
 
 ?>
